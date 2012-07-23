c12b15b2c8646f74762b0dcc6d2e28f0ee03be06
Mark Sinclair <125axel125@gmail.com>
2012-07-22 19:32:14 -0500

    RESTRUCTURED GAME CLASS; STARTED ADDING DRAWING
    The Game class now serves as a base class for the
    DrawGame class with graphical drawing added. Refactored
    SaveGame class properly, separated interface from
    implementation.
    
    TODO: Implement multijumping

---
+++ DrawGame.cpp
@@ -0,0 +1,91 @@
+/*
+ * DrawGame.cpp
+ *
+ *  Created on: Jul 22, 2012
+ *      Author: markjr
+ */
+
+#include <allegro.h>
+#include <iostream>
+
+#include "checkers.hpp"
+#include "player.hpp"
+#include "game.hpp"
+#include "DrawGame.hpp"
+
+BITMAP *buffer;
+
+DrawGame::DrawGame()
+: Game(false,false)
+{
+	const unsigned DRAW_SIZE = BOARD_SIZE * 30;
+	allegro_init();
+	install_keyboard();
+	set_gfx_mode( GFX_AUTODETECT_WINDOWED, DRAW_SIZE, DRAW_SIZE, 0, 0);
+	buffer = create_bitmap(DRAW_SIZE,DRAW_SIZE);
+	_print();
+}
+
+DrawGame::DrawGame(const SaveGame& record)
+: Game(record,false,false)
+{
+	const unsigned DRAW_SIZE = BOARD_SIZE * CELL_SIZE;
+	allegro_init();
+	install_keyboard();
+	set_gfx_mode( GFX_AUTODETECT_WINDOWED, DRAW_SIZE, DRAW_SIZE, 0, 0);
+	buffer = create_bitmap(DRAW_SIZE,DRAW_SIZE);
+	_print();
+}
+
+void DrawGame::_print() const
+{
+	const int TAN = makecol(0xff,0xda,0xb9);
+	const int BLACK = makecol(0x0,0x0,0x0);
+	const int RED = makecol(0xff,0x0,0x0);
+	const int SADDLEBROWN = makecol(0x8b,0x45,0x13);
+
+	clear_to_color(buffer,TAN);
+	/* Go through board, filling in cells according
+	 * to status
+	 */
+	unsigned xcord = 0, ycord = 0;
+	for (unsigned i = 0; i < BOARD_SIZE; i++) {
+		ycord = 0;
+		for (int j = BOARD_SIZE - 1; j >= 0; j--) {
+			if (!((i+j) % 2)) {
+				rectfill(buffer,xcord,ycord,xcord+CELL_SIZE, ycord+CELL_SIZE,SADDLEBROWN);
+			}
+			if ((*this)[i][j].getInPlay() ) {
+				int pieceCol;
+				if ( (*this)[i][j].getColor() == Piece::RED) {
+					pieceCol = RED;
+				} else {
+					pieceCol = BLACK;
+				}
+				circlefill(buffer,xcord + CELL_SIZE/2,ycord + CELL_SIZE/2, CELL_SIZE/2,pieceCol);
+			}
+			ycord += CELL_SIZE;
+		}
+		xcord += CELL_SIZE;
+	}
+	blit(buffer,screen,0,0,0,0,BOARD_SIZE * CELL_SIZE,BOARD_SIZE * CELL_SIZE);
+}
+
+DrawGame::~DrawGame()
+{
+	readkey();
+	destroy_bitmap(buffer);
+	allegro_exit();
+}
+END_OF_MAIN()
+
+int DrawGame::_receiveInput()
+{
+	return super::receiveInput();
+}
+
+int main()
+{
+	DrawGame *lol = new DrawGame();
+	delete lol;
+}
